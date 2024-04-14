34017df8c0cf94302f96ebc52bbe6f22f5f67823
82105 <ijh7476@naver.com>
2024-04-15 00:41:10 +0900

    - 익명클래스 연습

--- src/nested/ex/AnonymousMain.java
+++ src/nested/ex/AnonymousMain.java
@@ -4,6 +4,7 @@ public class AnonymousMain {
 
     public static void main(String[] args) {
 
+        // 익명 클래스를 생성하고 Hello 인터페이스를 구현
         Hello hello = new Hello() {
             @Override
             public void hello() {
