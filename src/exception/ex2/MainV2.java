1ffd4537913eaf265ab15f0fd15646004c175bef
82105 <ijh7476@naver.com>
2024-04-15 21:58:57 +0900

    - ex1패키지 예제 예외처리도입3 정상,예외 분리

--- src/exception/ex2/MainV2.java
+++ src/exception/ex2/MainV2.java
@@ -5,8 +5,12 @@ import java.util.Scanner;
 public class MainV2 {
     public static void main(String[] args) throws NetworkClientExceptionV2 { // 여기서도 예외를 밖으로 던져보자
 
-        NetworkServiceV2_1 networkService = new NetworkServiceV2_1();
+//        NetworkServiceV2_1 networkService = new NetworkServiceV2_1();
+//        NetworkServiceV2_2 networkService = new NetworkServiceV2_2();
+          NetworkServiceV2_3 networkService = new NetworkServiceV2_3();
+
         Scanner scanner = new Scanner(System.in);
+
 
         while (true) {
             System.out.print("전송할 문자 : ");
